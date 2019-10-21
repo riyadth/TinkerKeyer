8793bba67c179e137e50ff0bb34b24749238b33c
Riyadth Al-Kazily <riyadth@gmail.com>
2019-10-20 17:50:19 -0700

    Some musings on architecture for the code.
    Some operations will be done with direct register access, particularly
    the encoder and key/paddle input, because it is convenient to read all
    the values at once, and then debounce them for changes.

--- TinkerKeyer.ino
+++ TinkerKeyer.ino
@@ -86,6 +86,116 @@ class TinkerKit_LCD : public LiquidCrystal {
 
 TinkerKit_LCD lcd = TinkerKit_LCD();
 
+/**
+ * Function to read the paddle and key inputs, debounce them,
+ * and generate events when the inputs change.
+ */
+void _read_paddle_and_key(void)
+{
+  uint8_t port_d = PIND & 0b00101100;
+  uint8_t paddle = (port_d >> 2) & 0b00000011;
+  uint8_t key = (port_d >> 6) & 0b00000001;
+
+  // Debounce paddle and key inputs
+
+}
+
+/**
+ * Function to read the encoder bits and return them to the caller.
+ */
+uint8_t _read_encoder(void)
+{
+  // Read the bits of port B, and mask out all but the encoder bits
+  uint8_t encoder_bits = (PINB & 0b00001100) >> 2;
+
+  // TODO: Read the encoder button also
+  return encoder_bits;
+}
+
+/**
+ * Read the encoder inputs and update the encoder "counter".
+ * This routine must be called frequently enough to catch single-
+ * bit changes in the encoder output signal.
+ */
+static void _handle_encoder(void)
+{
+  static uint8_t last_encoder_bits = 0;
+
+  // Read the encoder bits and decide what to do with them
+  uint8_t encoder_bits = _read_encoder();
+  if (last_encoder_bits != encoder_bits)
+  {
+    // We have a "clock" and a "direction" bit to examine. Only evaluate
+    // the new state if just one of them has changed.
+  }
+}
+
+/**
+ * Read the potentiometer attached to the board, and return the
+ * raw 10-bit value to the caller.
+ */
+unsigned _read_potentiometer(void)
+{
+  return analogRead(A0);
+}
+
+/**
+ * Process the buttons connected via resistor ladder to the
+ * analog input.
+ */
+typedef enum {
+  BUTTON_NONE = 0,
+  BUTTON_1,
+  BUTTON_2,
+  BUTTON_3,
+  BUTTON_4,
+  BUTTON_5,
+  BUTTON_MAX
+} button_t;
+
+// Macro to compute approximate voltage levels for comparison
+#define V(x)  (unsigned)(((1023.0 * (float)(x)) / 5.0))
+
+/**
+ * Read the analog button input, and determine which (if any)
+ * button is pressed, returning that value to the caller.
+ */
+unsigned _read_buttons(void)
+{
+  unsigned button_value = BUTTON_NONE;
+
+  unsigned button_voltage = analogRead(A1);
+
+  // Maximum voltage is 1023 (10 bits)
+
+  if (button_voltage > V(4.5))
+  {
+    button_value = BUTTON_5;
+  }
+  else if (button_voltage > V(3.5))
+  {
+    button_value = BUTTON_4;
+  }
+  else if (button_voltage > V(2.5))
+  {
+    button_value = BUTTON_3;
+  }
+  else if (button_voltage > V(1.5))
+  {
+    button_value = BUTTON_2;
+  }
+  else if (button_voltage > V(0.5))
+  {
+    button_value = BUTTON_1;
+  }
+
+  return button_value;
+}
+
+
+/**
+ * One-time setup function
+ */
 void setup() {
   Serial.begin(9600);
   while (!Serial)
@@ -99,8 +209,30 @@ void setup() {
 
   lcd.print("Hello World!");
 
+  // Configure digital inputs and outputs
+  // The ICSP header (for the rotary encoder on port B
+  pinMode(16, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+  pinMode(15, INPUT_PULLUP);
+  // The straight key input
+  pinMode(6, INPUT_PULLUP);
+  // The paddle inputs
+  pinMode(0, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
+  // The keyed output signal
+  digitalWrite(A2, LOW);
+  pinMode(A2, OUTPUT);  // TODO: Need circuit to properly key the rig (optoisolator?)
+  // The front panel LED
+  digitalWrite(11, LOW);
+  pinMode(11, OUTPUT);
+  // The audio output
+  digitalWrite(5, LOW);
+  pinMode(5, OUTPUT);
 }
 
+/**
+ * Repeated loop function
+ */
 void loop() {
   // Blink the on-board LED
   lcd.boardLED(true);
